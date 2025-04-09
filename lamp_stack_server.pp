2d12929db2f0cecfdbca92f45eddb38c8bac08a0
Nick <znickmeyer@gmail.com>
2025-04-09 17:05:30 +0000

    Lab11

---
+++ lamp_stack_server.pp
@@ -0,0 +1,36 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure => installed,
+  require => Package['apache2'],  # Ensure Apache is installed first
+}
+
+package { 'libapache2-mod-php':
+  ensure => installed,
+  require => Package['php'],  # Ensure PHP is installed first
+}
+
+package { 'php-cli':
+  ensure => installed,
+  require => Package['php'],  # Ensure PHP is installed first
+}
+
+package { 'php-mysql':
+  ensure => installed,
+  require => Package['php'],  # Ensure PHP is installed first
+}
+
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [Package['apache2'], Package['php'], Package['libapache2-mod-php'], Package['php-cli']],
+}
+
+file { '/var/www/html/phpinfo.php':
+  source => 'file:///home/meye3469/nm_inet4031_puppet_lab9/phpinfo.php',  # Correct path
+  notify => Service['apache2'],  # Notify Apache to restart when the file changes
+  require => Package['apache2'],  # Ensure Apache is installed before creating the file
+}
+
